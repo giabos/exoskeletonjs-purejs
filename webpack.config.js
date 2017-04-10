972cfd7907c97a51710d3546a432909f531ac1cd
giabos <gianni.boscarino@gmail.com>
2017-04-10 20:52:09 +0000

    added uglify support + babel support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
@@ -6,8 +7,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+    ],
     module: {
         rules: [
+            {test: /\.(js|jsx)$/, use: 'babel-loader'},
             {test: /\.js$/, use: 'imports-loader?define=>false'},
         ]
     }
